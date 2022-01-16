b8354e174c471b4f5096680ea2769b99e664bb33
hopecris31 <hope.crisafi@gmail.com>
2022-01-15 23:31:38 -0500

    Initial commit

---
+++ cards.py
@@ -0,0 +1,42 @@
+"""
+shuffle deck
+take first 5 cards and make hand
+pop those cards the deck
+take 5 more cards until cant make another hand of 5
+"""
+
+"""
+flush =
+#flush, straight flush, royal flush
+two_pair =
+#two pair, four of a kind, full house
+pair =
+#pair, three of a kind
+high_card =
+#high card, straight
+"""
+
+
+"""
+modules:
+1. poker_sim.py (runs the actual percentages/chart)
+2. one that shuffles and deals cards
+3. one that identifies the hands
+4. 
+"""
+
+
+
+"""
+1. create standard deck (var, list)
+2. shuffle deck (def)
+3. deal hand (def, return var, list) (keep making hands of 5 until another cannot be made)
+4. identify hand category (def) (if hand category == type, add one to counter)
+5. keep count of hand categories (def)
+6. deal hands and sort until hands == 10,000 then stop
+7. create table
+8. output data into table
+9. complete process again for 20k, then 30k, etc., up to 100k (in increments of 10k)
+
+
+"""
---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+deck = ['1D', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD', 'AD',
+        '1H', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH', 'AH',
+        '1C', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC', 'AC',
+        '1S', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS', 'AS']
+
+
+
+def shuffle_deck(deck):
+        """
+        shuffles a deck of cards
+        :param deck: a deck of 52 cards
+        :return: the deck in random order
+        """
+        random.shuffle(deck)
+        return deck
+
